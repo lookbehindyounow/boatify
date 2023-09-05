b8c229a317a2515a6bb25d7f8bd3a40c14e3cbe0
weewilsonboy <wilson.euan@gmail.com>
2023-09-05 10:37:38 +0100

    added extras to the api

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,48 +2,47 @@ use boatify;
 db.dropDatabase();
 
 db.locations.insertMany([
-    {
+  {
     name:"Cala Pi de la Posada / Formentor Beach",
     travel_time:15,
     price_morning: 20,
     price_afternoon: 25,
     price_day: 35,
     price_base: 10,
-    },
-    {
+  },
+  {
     name:"Cala Sant Vicenc",
     travel_time:80,
     price_morning: 40,
     price_afternoon: 45,
     price_day: 75,
     price_base: 20,
-    },
-    {
+  },
+  {
     name:"S'Illot",
     travel_time:30,
     price_morning: 25,
     price_afternoon: 30,
     price_day: 45,
     price_base: 15,
-    }
+  }
 ]);
 db.extras.insertMany([
-    {
-        name:"Bacon Roll",
-        price:5
-    },
-    {
-        name:"Bucket of Beers",
-        price:25
-
-    },
-    {
-        name:"Champagne",
-        price:35
-    },
-    {
-        name:"Ice Lollies",
-        price:10
-    }
+  {
+    name:"Bacon Roll",
+    price:5
+  },
+  {
+    name:"Bucket of Beers",
+    price:25
+  },
+  {
+    name:"Champagne",
+    price:35
+  },
+  {
+    name:"Ice Lollies",
+    price:10
+  }
 
 ])
--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router.js");
 
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
   .then((client) => {
-    const db = client.db('boatify');
-    const locationsCollection = db.collection('locations');
+    const db = client.db("boatify");
+    const locationsCollection = db.collection("locations");
     const locationsRouter = createRouter(locationsCollection);
-    app.use('/api/locations', locationsRouter);
+    app.use("/api/locations", locationsRouter);
+    const extrasCollection = db.collection("extras");
+    const extrasRouter = createRouter(extrasCollection);
+    app.use("/api/extras", extrasRouter);
   })
   .catch(console.err);
 
 app.listen(7777, function () {
-  console.log(`Listening on port ${ this.address().port }`);
+  console.log(`Listening on port ${this.address().port}`);
 });
