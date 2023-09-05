cc47a7ba857ddca6580b09eb03388708d16c8814
Kieron Harrower <somebody.else.2017@gmail.com>
2023-09-05 10:07:12 +0100

    added fetch thig to link client & server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 function App() {
+  useEffect(()=>{fetching()},[])
+
+  const fetching = async()=>{
+    const collection = "locations"
+    const res = await fetch(`http://localhost:7777/api/${collection}`)
+    const data = await res.json()
+    console.log(data)
+  }
 
   return (
     <>
