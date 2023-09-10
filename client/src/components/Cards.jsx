3c01f08813327eccc63f4cfd0310614c799a66d8
weewilsonboy <wilson.euan@gmail.com>
2023-09-10 12:22:00 +0100

    added images as backgrounds, and made cards easier to read over images

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,16 +1,21 @@
 import Card from "./Card";
 import styled from "styled-components";
+import pollenca from "../images/pollenca-bay.jfif";
+import calaSanVicente from "../images/cala-san-vicente.jfif";
+import calaPi from "../images/cala-pi-de-la-posada.jfif";
 
 function Cards({ destinations, setStep, setBooking }) {
+  const imageRef = [calaPi, calaSanVicente, pollenca];
   return (
     <>
       <CardsView>
-        {destinations.map((destination,i) => (
+        {destinations.map((destination, i) => (
           <Card
             key={i}
             destination={destination}
             setStep={setStep}
             setBooking={setBooking}
+            imageRef={imageRef[i]}
           />
         ))}
       </CardsView>
