4306e551f3999f11d0ef7290ddc052ad5cdc4576
davidbujok <davebujok@gmail.com>
2023-10-05 10:38:36 +0100

    update register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 import CardPageStyle from "./CardPageStyle";
 
 export default function Register({ setStep, setUser }) {
-  const [logIn, setLogIn] = useState(false)
-  const [noUser, setNoUser] = useState(false)
+  const [logIn, setLogIn] = useState(false);
+  const [noUser, setNoUser] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("test@test.tt");
@@ -13,18 +13,15 @@ export default function Register({ setStep, setUser }) {
   const [tooShort, setTooShort] = useState(false);
   const [special, setSpecial] = useState(1);
   const [fieldPasswordEmpty, setFieldPasswordEmpty] = useState("");
-  const emailPattern = /^[a-zA-Z0-9._%+-]+\@[a-zA-Z.-_0-9]+\.[a-z]{2,}$/;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+\@[a-zA-Z.-_0-9]+\.[a-z.]{2,}$/;
 
   async function getUser() {
-    const res=await fetch("http://localhost:7777/api/users")
-    const data=await res.json()
-    const attemptedUser=data.find(user=>user.username==username && user.password==password)
-    attemptedUser ? (
-      setUser(attemptedUser),
-      setStep(-2)
-    ) : (
-      setNoUser(true)
-    )
+    const res = await fetch("http://localhost:7777/api/users");
+    const data = await res.json();
+    const attemptedUser = data.find(
+      (user) => user.username == username && user.password == password
+    );
+    attemptedUser ? (setUser(attemptedUser), setStep(-2)) : setNoUser(true);
   }
 
   function formValidation() {
@@ -85,8 +82,9 @@ export default function Register({ setStep, setUser }) {
         method: "POST",
         body: JSON.stringify(user),
         headers: { "Content-Type": "application/json" },
-      }).then((res) => res.json())
-        .then(data=>setUser({...user,_id:data._id}))
+      })
+        .then((res) => res.json())
+        .then((data) => setUser({ ...user, _id: data._id }));
       setStep(-2);
     }
   }
@@ -94,7 +92,7 @@ export default function Register({ setStep, setUser }) {
   return (
     <>
       <Page>
-        <br/>
+        <br />
         <h1
           style={{
             fontSize: "35px",
@@ -102,11 +100,14 @@ export default function Register({ setStep, setUser }) {
             color: "#2f86c5",
           }}
         >
-          {logIn?"Log in":"Register"}
+          {logIn ? "Log in" : "Register"}
         </h1>
-        <Button title={logIn?"I'm new":"Log in"} action={()=>setLogIn(!logIn)}/>
+        <Button
+          title={logIn ? "I'm new" : "Log in"}
+          action={() => setLogIn(!logIn)}
+        />
         <CardPageStyle>
-          <form action="#" onSubmit={logIn?getUser:formValidation}>
+          <form action="#" onSubmit={logIn ? getUser : formValidation}>
             <h2 style={{ fontWeight: 300, color: "#2c7172" }}>Username</h2>
             <input
               style={{
@@ -119,7 +120,7 @@ export default function Register({ setStep, setUser }) {
               type="text"
               value={username}
               onChange={(e) => {
-                setNoUser(false)
+                setNoUser(false);
                 const value = e.target.value;
                 setUsername(value);
               }}
@@ -131,6 +132,7 @@ export default function Register({ setStep, setUser }) {
             ) : null}
             <h2 style={{ fontWeight: 300, color: "#2c7172" }}>Password</h2>
             <input
+              autoComplete="true"
               style={{
                 fontSize: "25px",
                 border: "none",
@@ -140,7 +142,7 @@ export default function Register({ setStep, setUser }) {
               }}
               type="password"
               onChange={(e) => {
-                setNoUser(false)
+                setNoUser(false);
                 setPassword(e.target.value);
               }}
             />
@@ -159,32 +161,45 @@ export default function Register({ setStep, setUser }) {
                 Password must have at least 1 special character
               </p>
             ) : null}
-            {logIn?null:<>
-            <h2 style={{ fontWeight: 300, color: "#2c7172" }}>E-mail</h2>
-              <input
-                style={{
-                  fontSize: "25px",
-                  border: "none",
-                  padding: "5px",
-                  marginTop: "5px",
-                  color: "#144c74",
-                }}
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {!emailPattern.test(email) ? (
-                <p style={{ color: "red", fontSize: "14px" }}>
-                  Invalid email address
-                </p>
-              ) : null}
-            </>}
-            {noUser?<p style={{ color: "red", fontSize: "14px" }}>No user with those details</p>:null}
-            <div style={{marginTop: "25px", alignSelf: "center", display: "flex", justifyContent: "center"}}>
+            {logIn ? null : (
+              <>
+                <h2 style={{ fontWeight: 300, color: "#2c7172" }}>E-mail</h2>
+                <input
+                  style={{
+                    fontSize: "25px",
+                    border: "none",
+                    padding: "5px",
+                    marginTop: "5px",
+                    color: "#144c74",
+                  }}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {!emailPattern.test(email) ? (
+                  <p style={{ color: "red", fontSize: "14px" }}>
+                    Invalid email address
+                  </p>
+                ) : null}
+              </>
+            )}
+            {noUser ? (
+              <p style={{ color: "red", fontSize: "14px" }}>
+                No user with those details
+              </p>
+            ) : null}
+            <div
+              style={{
+                marginTop: "25px",
+                alignSelf: "center",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
               <Button type="submit" title={"Confirm"} />
             </div>
           </form>
         </CardPageStyle>
-        <Button title="back" color="#144c74" action={()=>setStep(0)}/>
+        <Button title="back" color="#144c74" action={() => setStep(0)} />
       </Page>
     </>
   );
